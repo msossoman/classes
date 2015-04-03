ee2645982b8bab344aa18b622d7354b1001506b0
Mark Sossoman <msossoman@gmail.com>
2015-04-03 11:51:41 -0400

    setup classes

---
+++ bandCL.py
@@ -0,0 +1,14 @@
+class Band(object):
+
+	def __init__(self, name, instrument):
+		self.name = name
+		self.instrument = instrument
+
+	def getName(self):
+		return self.name
+
+	def getInstrument(self):
+		return self.instrument
+
+	def __str__(self):
+		return "%s plays the %s." % (self.name, self.instrument)
