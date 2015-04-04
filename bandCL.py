9ac1997886e015e5d209998200a7406d89325e37
Mark Sossoman <msossoman@gmail.com>
2015-04-03 20:45:01 -0400

    temp change to bandCL and setup vehicle

--- bandCL.py
+++ bandCL.py
@@ -2,7 +2,7 @@ class Band(object):
 
 	def __init__(self, name, instrument):
 		self.name = name
-		self.instrument = instrument
+		'''self.instrument = instrument
 
 	def getName(self):
 		return self.name
@@ -11,4 +11,4 @@ class Band(object):
 		return self.instrument
 
 	def __str__(self):
-		return "%s plays the %s." % (self.name, self.instrument)
+		return "%s plays the %s." % (self.name, self.instrument)'''
